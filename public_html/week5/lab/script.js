301a0924ae20368333841f205018e3ad70639d51
001309265 <001309265@N205A2.labs.neit.edu>
2014-08-26 19:50:11 -0400

    lab 5 commit

--- public_html/week5/lab/script.js
+++ public_html/week5/lab/script.js
@@ -17,9 +17,17 @@
  */
 
 
+
+
+
 function SpaceAlphaValidate( str ) {
         var alphaRegex = /[a-zA-Z ]+/;
         return alphaRegex.test(str);			
+}
+
+function emailValidate(str) {
+     var emailRegex = /^[a-zA-Z]+[@]{1}[a-zA-Z]+[\.]{1}[a-zA-Z]{2,3}$/;
+     return emailRegex.test(str);
 }
 
 function strip_HTML(str) {
@@ -27,16 +35,53 @@ function strip_HTML(str) {
         return str.replace(findHtml,"");
 }
 
-function submitform() {
+function submitForm() {
 			
         var fname = document.getElementById('fname');
+        var lastName = document.getElementById("lname");
+        var emailAddr = document.getElementById("email");
+        var txtComments = document.getElementById("comments");
+        var fname_err = document.getElementById("err_fname");
+        var lname_err = document.getElementById("err_lname");
+        var emailAddr_err = document.getElementById("err_email");
+        var txtCommentsErr = document.getElementById("err_comments");
+        var formDiv = document.getElementById("content");
+        var resultsDiv = document.getElementById("results");
+
 
         if ( !fname.value.length ) {
-                console.log("Fname needs a length");
-        } else if ( SpaceAlphaValidate( fname.value ) === false ) {
-                console.log("Fname needs Alpha chars");
+                fname_err.innerHTML = "Fname needs a length";
+        } else if ( SpaceAlphaValidate(fname.value) === false ) {
+                fname_err.innerHTML = "Fname needs Alpha chars";
         } else {
-                console.log("Fname is good");
+                fname_err.innerHTML = "Fname is good";
         }
+        
+        
+        
+        if (!lastName.value.length) {
+            lname_err.innerHTML = "Lname needs a length";
+        } else if ( SpaceAlphaValidate(lastName.value) === false ) {
+            lname_err.innerHTML = "Lname needs alpha chars";
+        } else {
+            lname_err.innerHTML  = "Lname is good!";
+        }
+        
+        if (!emailAddr.value.length) {
+            emailAddr_err.innerHTML = "email needs a length";
+        } else if (emailValidate(emailAddr.value) === false ) {
+            emailAddr_err.innerHTML = "invalid email. please use the email@domain.tld format";
+        } else {
+
+            emailAddr_err.innerHTML = "email is good!";
+        }
+        
+        txtComments.value = strip_HTML(txtComments.value);
+        if ( SpaceAlphaValidate(fname.value) === true && emailValidate(emailAddr.value) === true &&  SpaceAlphaValidate(lastName.value) === true )
+        {
+            formDiv.style.display = 'none';
+            resultsDiv.innerHTML = 'First and Last Name: ' + fname.value + lastName.value + '</br>Email Address: ' + emailAddr.value + '</br>Comments: ' + txtComments.value;
+        }
+        console.log(txtComments.value);
 
 }
