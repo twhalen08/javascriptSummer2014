12ed9b79e57e630eaedc8218d0fcb45a116d55fa
001309265 <001309265@N205A2.labs.neit.edu>
2014-09-16 17:57:57 -0400

    lab4 commit

--- public_html/Week4/lab/script.js
+++ public_html/Week4/lab/script.js
@@ -25,14 +25,48 @@ mouse cords into the array.
 
  */
 
+//userdata is the json that stores all of the data we're collecting. 
+var userdata = {
+    "x" : [],// mouse x axis
+    "y" : [], // mouse y axis
+    "agent" : "", // useragent
+    "height" : "", //window height
+    "width" : "", //window width
+    "title" : "", //page title
+    "email" : "", //email field
+    "username" : "" //name field
+};
+userDat(); //userdat is called right after the jason is created and stores all of the window and useragent info. 
 
-var userdata = {};
+function trackMouse(e) { //the trackmouse takes the users mouses x and y positions and writes them to the json until 100 have been collected
+    userdata.x.push(e.clientX);
+    userdata.y.push(e.clientY);
+     if ( userdata.x.length >= 100 && userdata.y.length >= 100) {
+                    document.removeEventListener('mousemove', trackMouse);
+                }
 
-function showResults() {
-    console.clear();
+}
+function userDat() { // this function as stated above collects the useragent and window data and writes it to the json
+    userdata.agent = window.navigator.userAgent;
+    userdata.height = window.innerHeight;
+    userdata.width = window.innerWidth;
+    userdata.title = document.title;
+}
+function onBlur1() {// when the user blurs out of the fullname field, the fullname is stored
+   userdata.username = fullname.value;
+}
+function onBlur2() { //when the user blurs out of the email field, the email is stored.
+    userdata.email = email.value;
+}
+function showResults() { // writes all results to the console and is called when the button is clicked
     console.log(userdata);
 }
+var fullname = document.getElementById("fullname");
+var email = document.getElementById("email");
+fullname.addEventListener("blur", onBlur1);
+email.addEventListener("blur", onBlur2);
 
 var results = document.getElementById('showResults');
 
 results.addEventListener("click", showResults);
+document.addEventListener('mousemove', trackMouse);
